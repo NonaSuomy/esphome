e4044e52094a967388dd1d82cd88c03485ad9ab8
NonaSuomy <nonasuomy@live.com>
2024-06-07 09:20:55 -0400

    Update vl6180x_sensor.cpp
    write to writing
    read to reading

--- esphome/components/vl6180x/vl6180x_sensor.cpp
+++ esphome/components/vl6180x/vl6180x_sensor.cpp
@@ -27,7 +27,7 @@ void VL6180XSensor::setup() {
 }
 
 void VL6180XSensor::update() {
-  this->write_register(0x018, 0x01); // Start a new measurement
+  this->writing_register(0x018, 0x01); // Start a new measurement
   this->data_ready_ = true; // Set the flag to indicate that data is expected
 }
 
@@ -49,12 +49,12 @@ void VL6180XSensor::loop() {
   // If data is expected, check if it's ready
   if (!this->data_ready_)
     return;
-  if (!(this->read_register(0x04f) & 0x04))
+  if (!(this->reading_register(0x04f) & 0x04))
     return;
   this->data_ready_ = false; // Reset the flag as we've read the data
 
-  uint8_t range = this->read_register(0x062);
-  this->write_register(0x015, 0x07);  // Clear the interrupt
+  uint8_t range = this->reading_register(0x062);
+  this->writing_register(0x015, 0x07);  // Clear the interrupt
   distance_sensor_->publish_state(range);
 	
   // Read the ALS value from the sensor
@@ -126,59 +126,59 @@ void VL6180XSensor::load_settings() {
   // Mandatory Private Registors from Section 9 page 24 of application note 
   // required to be loaded onto the VL6180X during the initialisation of the device.
   // https://www.st.com/resource/en/application_note/an4545-vl6180x-basic-ranging-application-note-stmicroelectronics.pdf
-  write_register(0x0207, 0x01);
-  write_register(0x0208, 0x01);
-  write_register(0x0096, 0x00);
-  write_register(0x0097, 0xfd);
-  write_register(0x00e3, 0x00);
-  write_register(0x00e4, 0x04);
-  write_register(0x00e5, 0x02);
-  write_register(0x00e6, 0x01);
-  write_register(0x00e7, 0x03);
-  write_register(0x00f5, 0x02);
-  write_register(0x00d9, 0x05);
-  write_register(0x00db, 0xce);
-  write_register(0x00dc, 0x03);
-  write_register(0x00dd, 0xf8);
-  write_register(0x009f, 0x00);
-  write_register(0x00a3, 0x3c);
-  write_register(0x00b7, 0x00);
-  write_register(0x00bb, 0x3c);
-  write_register(0x00b2, 0x09);
-  write_register(0x00ca, 0x09);
-  write_register(0x0198, 0x01);
-  write_register(0x01b0, 0x17);
-  write_register(0x01ad, 0x00);
-  write_register(0x00ff, 0x05);
-  write_register(0x0100, 0x05);
-  write_register(0x0199, 0x05);
-  write_register(0x01a6, 0x1b);
-  write_register(0x01ac, 0x3e);
-  write_register(0x01a7, 0x1f);
-  write_register(0x0030, 0x00);
+  writing_register(0x0207, 0x01);
+  writing_register(0x0208, 0x01);
+  writing_register(0x0096, 0x00);
+  writing_register(0x0097, 0xfd);
+  writing_register(0x00e3, 0x00);
+  writing_register(0x00e4, 0x04);
+  writing_register(0x00e5, 0x02);
+  writing_register(0x00e6, 0x01);
+  writing_register(0x00e7, 0x03);
+  writing_register(0x00f5, 0x02);
+  writing_register(0x00d9, 0x05);
+  writing_register(0x00db, 0xce);
+  writing_register(0x00dc, 0x03);
+  writing_register(0x00dd, 0xf8);
+  writing_register(0x009f, 0x00);
+  writing_register(0x00a3, 0x3c);
+  writing_register(0x00b7, 0x00);
+  writing_register(0x00bb, 0x3c);
+  writing_register(0x00b2, 0x09);
+  writing_register(0x00ca, 0x09);
+  writing_register(0x0198, 0x01);
+  writing_register(0x01b0, 0x17);
+  writing_register(0x01ad, 0x00);
+  writing_register(0x00ff, 0x05);
+  writing_register(0x0100, 0x05);
+  writing_register(0x0199, 0x05);
+  writing_register(0x01a6, 0x1b);
+  writing_register(0x01ac, 0x3e);
+  writing_register(0x01a7, 0x1f);
+  writing_register(0x0030, 0x00);
   // Recommended : Public registers - See data sheet for more detail
-  write_register(0x0011, 0x10); // Enables polling for 'New Sample ready'
+  writing_register(0x0011, 0x10); // Enables polling for 'New Sample ready'
                                 // when measurement completes
-  write_register(0x010a, 0x30); // Set the averaging sample period
+  writing_register(0x010a, 0x30); // Set the averaging sample period
                                 // (compromise between lower noise and
                                 // increased execution time)
-  write_register(0x003f, 0x46); // Sets the light and dark gain (upper
+  writing_register(0x003f, 0x46); // Sets the light and dark gain (upper
                                 // nibble). Dark gain should not be
                                 // changed.
-  write_register(0x0031, 0xFF); // Sets the # of range measurements after
+  writing_register(0x0031, 0xFF); // Sets the # of range measurements after
                                 // which auto calibration of system is performed
-  write_register(0x0041, 0x63); // Set ALS integration time to 100ms
-  write_register(0x002e, 0x01); // Perform a single temperature calibration of the ranging sensor
+  writing_register(0x0041, 0x63); // Set ALS integration time to 100ms
+  writing_register(0x002e, 0x01); // Perform a single temperature calibration of the ranging sensor
   
-  write_register(0x0014, 0x04); // Enables the notification of a new value being available.
-  //write_register(0x001b, 0x80); // continuous mode interval 
+  writing_register(0x0014, 0x04); // Enables the notification of a new value being available.
+  //writing_register(0x001b, 0x80); // continuous mode interval 
   
   // Optional: Public registers - See data sheet for more detail
-  //write_register(0x001b, 0x09); // Set default ranging inter-measurement
+  //writing_register(0x001b, 0x09); // Set default ranging inter-measurement
                                   // period to 100ms
-  //write_register(0x003e, 0x31); // Set default ALS inter-measurement period
+  //writing_register(0x003e, 0x31); // Set default ALS inter-measurement period
                                   // to 500ms
-  //write_register(0x0014, 0x24); // Configures interrupt on 'New Sample
+  //writing_register(0x0014, 0x24); // Configures interrupt on 'New Sample
                                   // Ready threshold event'
 }
 // Assuming there the device is in a 200lux environment, by starting a single shot ambient
