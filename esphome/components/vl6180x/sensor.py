b3f582e5e59b1ab598cd71d4a7d2eea311c0155f
NonaSuomy <nonasuomy@live.com>
2024-06-10 02:17:06 -0400

    Update sensor.py
    gesture test

--- esphome/components/vl6180x/sensor.py
+++ esphome/components/vl6180x/sensor.py
@@ -28,17 +28,25 @@ VL6180XSensor = vl6180x_ns.class_(
 CONF_DISTANCE = "distance"
 CONF_ALS = "als"
 CONF_GAIN = "gain"
-CONF_ON_SWIPE_GESTURE = "on_swipe_gesture"
-CONF_ON_DOUBLE_TAP_GESTURE = "on_double_tap_gesture"
-CONF_ON_HOVER_GESTURE = "on_hover_gesture"
 CONF_UNDER_GLASS = "under_glass"
 CONF_LUX_WITHOUT_GLASS = "lux_without_glass"
+# CONF_ON_GESTURE_SWIPE_TOWARD = "on_gesture_swipe_toward"
+# CONF_ON_GESTURE_SWIPE_AWAY = "on_gesture_swipe_away"
+# CONF_ON_GESTURE_SINGLE_TAP = "on_gesture_single_tap"
+# CONF_ON_GESTURE_DOUBLE_TAP = "on_gesture_double_tap"
+# CONF_ON_GESTURE_HOVER = "on_gesture_hover"
+# CONF_GESTURE_SINGLE_TAP_INTERVAL_MS = "gesture_single_tap_interval_ms"
+# CONF_GESTURE_DOUBLE_TAP_INTERVAL_MS = "gesture_double_tap_interval_ms"
+# CONF_GESTURE_DOUBLE_TAP_DELAY_MS = "gesture_double_tap_delay_ms"
+# CONF_GESTURE_HOVER_TIME_MS = "gesture_hover_time_ms"
+# CONF_GESTURE_HAND_PRESENCE_THRESHOLD = "gesture_hand_presence_threshold"
+# CONF_GESTURE_DOUBLE_TAP_THRESHOLD = "gesture_double_tap_threshold"
 
-def validate(config):
-    if CONF_DISTANCE not in config and CONF_ALS not in config:
-        raise cv.Invalid("You must select at least one of distance or als.")
-    return config
-    
+#def validate(config):
+#    if CONF_DISTANCE not in config and CONF_ALS not in config:
+#        raise cv.Invalid("You must select at least one of distance or als.")
+#    return config
+
 CONFIG_SCHEMA = cv.All(
   cv.Schema(
     {
@@ -49,44 +57,42 @@ CONFIG_SCHEMA = cv.All(
         accuracy_decimals=0,
         device_class=DEVICE_CLASS_DISTANCE,
       ),
-      cv.Optional(CONF_ALS): cv.Schema({
-        cv.Required('name'): cv.string,
-        cv.Optional('gain', default='5X'): cv.enum({
-          '1x': 0x06,
-          '1.25x': 0x05,
-          '1.67x': 0x04,
-          '2.5x': 0x03,
-          '5x': 0x02,
-          '10x': 0x01,
-          '20x': 0x00,
-          '40x': 0x07,
-        }, lower=True),
-        cv.Optional(CONF_UNDER_GLASS): cv.boolean,
-        cv.Optional(CONF_LUX_WITHOUT_GLASS): cv.float_, 
-      }
-      )
-      .extend(
-        sensor.sensor_schema(
+      cv.Optional(CONF_ALS): sensor.sensor_schema(
           unit_of_measurement=UNIT_LUX,
           icon=ICON_BRIGHTNESS_5,
           accuracy_decimals=1,
           device_class=DEVICE_CLASS_ILLUMINANCE,
-        )
+      ).extend(
+        {
+          cv.Optional('gain', default='1X'): cv.enum({
+            '1x': 0x06,
+            '1.25x': 0x05,
+            '1.67x': 0x04,
+            '2.5x': 0x03,
+            '5x': 0x02,
+            '10x': 0x01,
+            '20x': 0x00,
+            '40x': 0x07,
+          }, lower=True),
+          cv.Optional(CONF_UNDER_GLASS): cv.boolean,
+          cv.Optional(CONF_LUX_WITHOUT_GLASS): cv.float_,
+        }
       ),
-      cv.Optional(CONF_ON_SWIPE_GESTURE): automation.validate_automation({
-          cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(Automation),
-      }),
-      cv.Optional(CONF_ON_DOUBLE_TAP_GESTURE): automation.validate_automation({
-          cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(Automation),
-      }),
-      cv.Optional(CONF_ON_HOVER_GESTURE): automation.validate_automation({
-          cv.GenerateID(CONF_TRIGGER_ID): cv.declare_id(Automation),
-      }),
+      # cv.Optional(CONF_ON_GESTURE_SWIPE_TOWARD): automation.validate_automation(single=True),
+      # cv.Optional(CONF_ON_GESTURE_SWIPE_AWAY): automation.validate_automation(single=True),
+      # cv.Optional(CONF_ON_GESTURE_SINGLE_TAP): automation.validate_automation(single=True),
+      # cv.Optional(CONF_ON_GESTURE_DOUBLE_TAP): automation.validate_automation(single=True),
+      # cv.Optional(CONF_ON_GESTURE_HOVER): automation.validate_automation(single=True),
+      # cv.Optional(CONF_GESTURE_SINGLE_TAP_INTERVAL_MS, default=500): cv.uint32_t,
+      # cv.Optional(CONF_GESTURE_DOUBLE_TAP_INTERVAL_MS, default=500): cv.uint32_t,
+      # cv.Optional(CONF_GESTURE_DOUBLE_TAP_DELAY_MS, default=1000): cv.uint32_t,
+      # cv.Optional(CONF_GESTURE_HOVER_TIME_MS, default=3000): cv.uint32_t,
+      # cv.Optional(CONF_GESTURE_HAND_PRESENCE_THRESHOLD, default=40): cv.uint8_t,
+      # cv.Optional(CONF_GESTURE_DOUBLE_TAP_THRESHOLD, default=40): cv.uint8_t,
     }
   )
   .extend(cv.polling_component_schema('60s'))
   .extend(i2c.i2c_device_schema(0x29)),
-  cv.has_at_least_one_key(CONF_DISTANCE, CONF_ALS),
 )
 
 async def to_code(config):
@@ -96,28 +102,32 @@ async def to_code(config):
   if CONF_DISTANCE in config:
     distance = await sensor.new_sensor(config[CONF_DISTANCE])
     cg.add(var.set_distance_sensor(distance))
+  # Register the gesture triggers
+  # if CONF_ON_GESTURE_SWIPE_TOWARD in config:
+    # await automation.build_automation(
+      # var.get_gesture_swipe_toward_trigger(), [], config[CONF_ON_GESTURE_SWIPE_TOWARD]
+    # )
+  # if CONF_ON_GESTURE_SWIPE_AWAY in config:
+    # await automation.build_automation(
+      # var.get_gesture_swipe_away_trigger(), [], config[CONF_ON_GESTURE_SWIPE_AWAY]
+    # )
+  # if CONF_ON_GESTURE_SINGLE_TAP in config:
+    # await automation.build_automation(
+      # var.get_gesture_single_tap_trigger(), [], config[CONF_ON_GESTURE_SINGLE_TAP]
+    # )
+  # if CONF_ON_GESTURE_DOUBLE_TAP in config:
+    # await automation.build_automation(
+      # var.get_gesture_double_tap_trigger(), [], config[CONF_ON_GESTURE_DOUBLE_TAP]
+    # )
+  # if CONF_ON_GESTURE_HOVER in config:
+    # await automation.build_automation(
+      # var.get_gesture_hover_trigger(), [], config[CONF_ON_GESTURE_HOVER]
+    # )
   if CONF_ALS in config:
     als = await sensor.new_sensor(config[CONF_ALS])
     cg.add(var.set_als_sensor(als))
     cg.add(var.set_gain(config[CONF_ALS][CONF_GAIN]))
-    if CONF_UNDER_GLASS in config[CONF_ALS]:  
+    if CONF_UNDER_GLASS in config[CONF_ALS]:
       cg.add(var.set_is_behind_glass(config[CONF_ALS][CONF_UNDER_GLASS]))
-    if CONF_LUX_WITHOUT_GLASS in config[CONF_ALS]:  # Add this line
+    if CONF_LUX_WITHOUT_GLASS in config[CONF_ALS]:
       cg.add(var.set_lux_without_glass(config[CONF_ALS][CONF_LUX_WITHOUT_GLASS]))
-  # Register the gesture triggers
-  #if CONF_ON_SWIPE_GESTURE in config:
-  #  for conf in config[CONF_ON_SWIPE_GESTURE]:
-  #      trigger = var.get_swipe_gesture_trigger()
-  #      await automation.build_automation(trigger, [], conf)
-  #if CONF_ON_SWIPE_GESTURE in config:
-  #  for conf in config[CONF_ON_SWIPE_GESTURE]:
-  #    trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
-  #    await automation.build_automation(trigger, [(int, 'x')], conf)
-  #if CONF_ON_DOUBLE_TAP_GESTURE in config:
-  #  for conf in config[CONF_ON_DOUBLE_TAP_GESTURE]:
-  #    trigger = var.get_double_tap_gesture_trigger()
-  #    await automation.build_automation(trigger, [], conf)
-  #if CONF_ON_HOVER_GESTURE in config:
-  #  for conf in config[CONF_ON_HOVER_GESTURE]:
-  #    trigger = var.get_hover_gesture_trigger()
-  #    await automation.build_automation(trigger, [], conf)
