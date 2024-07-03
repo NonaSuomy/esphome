19bce9381749a645201d94aeb574eee105510717
NonaSuomy <nonasuomy@live.com>
2024-07-03 03:46:46 -0400

    Create yubikey_otp.h

---
+++ esphome/components/yubikey_otp/yubikey_otp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/pn532/pn532.h"
+
+namespace esphome {
+namespace yubikey_otp {
+
+class YubikeyOtpSensor;
+
+}  // namespace yubikey_otp
+}  // namespace esphome
+
+#include "yubikey_sensor.h"
