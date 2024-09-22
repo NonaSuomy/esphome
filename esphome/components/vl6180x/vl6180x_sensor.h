949ea729d8d22270ff4e08130ff4ecf5780a1c47
NonaSuomy <nonasuomy@live.com>
2024-09-22 06:16:38 -0400

    Update vl6180x_sensor.h

--- esphome/components/vl6180x/vl6180x_sensor.h
+++ esphome/components/vl6180x/vl6180x_sensor.h
@@ -5,12 +5,13 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/automation.h"
+#include <cstdint>
 
 namespace esphome {
 namespace vl6180x {
 
 // Register addresses
-enum VL6180XRegister {
+enum class VL6180XRegister : uint16_t {
   VL6180X_DEFAULT_I2C_ADDR = 0x29, // The fixed I2C address
 
   // Device model identification number
@@ -21,6 +22,12 @@ enum VL6180XRegister {
   VL6180X_REG_SYSTEM_INTERRUPT_CLEAR = 0x015,
   // Fresh out of reset bit
   VL6180X_REG_SYSTEM_FRESH_OUT_OF_RESET = 0x016,
+
+  VL6180X_REG_RANGE_SCALER = 0x096,
+
+  VL6180X_REG_SYSRANGE_CROSSTALK_VALID_HEIGHT = 0x021,
+
+  VL6180X_REG_SYSRANGE_RANGE_CHECK_ENABLES = 0x02D,
   // Trigger Ranging
   VL6180X_REG_SYSRANGE_START = 0x018,
   // Part to part range offset
@@ -45,8 +52,17 @@ enum VL6180XRegister {
   VL6180X_REG_SLAVE_DEVICE_ADDRESS = 0x212,
 };
 
+// Scalar
+enum class VL6180XScalerFactor : uint8_t {
+  // Sensor scale factor
+  // The higher the value the less acurate.
+  VL6180X_SCALER_FACTOR_1 = 0xFD, // 1x Scaler Factor 0mm to 20mm with a granularity of 1mm
+  VL6180X_SCALER_FACTOR_2 = 0x7F, // 2x Scaler Factor 0mm to 40mm with a granularity of 2mm
+  VL6180X_SCALER_FACTOR_3 = 0x54, // 3x Scaler Factor 0mm to 60mm with a granularity of 3mm
+};
+
 // ALS Sensor gain levels
-enum VL6180XALSGain {
+enum class VL6180XALSGain {
   // Sensor gain levels
   // For better results at low light,  use higher gain.
   // For better results at high light, use a lower gain.
@@ -81,6 +97,7 @@ enum VL6180XError {
   VL6180X_ERROR_RANGINGFILTERED_16 = 16, // Distance filtered by Wrap Around Filter (WAF). Occurs when a high reflectance target is detected between 600mm to 1.2m
   // 17                                  // Not used
   VL6180X_ERROR_DATANOTREADY_18 = 18,    // Error returned by VL6180x_RangeGetMeasurementIfReady() when ranging data is not ready.
+  VL6180X_ERROR_SENSORVERSIONNOTFOUND_19 = 19,  // Custom error to show no version found.
 };
 
 // VL6180X Sensor class
@@ -94,6 +111,7 @@ class VL6180XSensor : public sensor::Sensor, public PollingComponent, public i2c
     };
 
     float get_setup_priority() const override { return setup_priority::DATA; }
+    uint8_t get_scaling() const { return scaling_; }
 
 	// Constructor
     VL6180XSensor(uint8_t address = 0x29, uint32_t update_interval = 60000);
@@ -106,8 +124,9 @@ class VL6180XSensor : public sensor::Sensor, public PollingComponent, public i2c
 
     // Sensor configuration methods
     void set_distance_sensor(sensor::Sensor *distance_sensor) { distance_sensor_ = distance_sensor; }
+    void set_scaling(uint8_t new_scaling);
     void set_als_sensor(sensor::Sensor *als_sensor) { als_sensor_ = als_sensor; }
-    void set_gain(int gain) { gain_ = gain; }
+    void set_gain(VL6180XALSGain gain) { gain_ = gain; }
     void set_is_behind_glass(bool is_behind_glass) { is_behind_glass_ = is_behind_glass; }
     void set_lux_without_glass(float lux_without_glass) { lux_without_glass_ = lux_without_glass; }
 
@@ -118,7 +137,7 @@ class VL6180XSensor : public sensor::Sensor, public PollingComponent, public i2c
     void set_gesture_hover_time_ms(uint32_t gesture_hover_time_ms) { gesture_hover_time_ms_ = gesture_hover_time_ms; }
     void set_gesture_hand_presence_threshold(uint8_t gesture_hand_presence_threshold) { gesture_hand_presence_threshold_ = gesture_hand_presence_threshold; }
     void set_gesture_double_tap_threshold(uint8_t gesture_double_tap_threshold) { gesture_double_tap_threshold_ = gesture_double_tap_threshold; } */
-	
+
 	// Sensor operation methods
     void start_measurement();
     void check_measurement();
@@ -130,10 +149,10 @@ class VL6180XSensor : public sensor::Sensor, public PollingComponent, public i2c
 	Trigger<> *get_gesture_single_tap_trigger() const { return on_gesture_single_tap_trigger_; }
 	Trigger<> *get_gesture_double_tap_trigger() const { return on_gesture_double_tap_trigger_; }
 	Trigger<> *get_gesture_hover_trigger() const { return on_gesture_hover_trigger_; } */
-  
+
     // Declare a boolean variable to indicate whether the sensor is behind a glass cover
     bool is_behind_glass_ = false;
-  
+
     // Register read/write methods
     void writing_register(uint16_t reg, uint8_t data);
     uint8_t reading_register(uint16_t reg);
@@ -144,15 +163,29 @@ class VL6180XSensor : public sensor::Sensor, public PollingComponent, public i2c
     uint8_t read_range();
     float read_als(uint8_t gain); // Read the ALS value
 
+  private:
+    uint8_t scaling_{1};  // Default to 1x scaling
+    int8_t ptp_offset_{0};  // Part-to-part offset
+    static const uint16_t ScalerValues[4];
+    uint8_t get_scaler_value() const {
+      switch (scaling_) {
+        case 1: return static_cast<uint8_t>(VL6180XScalerFactor::VL6180X_SCALER_FACTOR_1);
+        case 2: return static_cast<uint8_t>(VL6180XScalerFactor::VL6180X_SCALER_FACTOR_2);
+        case 3: return static_cast<uint8_t>(VL6180XScalerFactor::VL6180X_SCALER_FACTOR_3);
+        default: return static_cast<uint8_t>(VL6180XScalerFactor::VL6180X_SCALER_FACTOR_1);  // Default to 1x if invalid
+      }
+    }
+
   protected:
     // Sensor state and configuratoin variables
     bool data_ready_{false};
-    int gain_;
+    VL6180XALSGain gain_{VL6180XALSGain::VL6180X_ALS_GAIN_1}; // Initialize with default gain
     sensor::Sensor *distance_sensor_;
     sensor::Sensor *als_sensor_;
     float lux_without_glass_;
     float als_lux_resolution_without_glass_;
-	
+    float read_als(); // Update the function signature
+
 /* 	uint32_t gesture_single_tap_interval_ms_;
 	uint32_t gesture_double_tap_interval_ms_;
 	uint32_t gesture_single_tap_delay_ms_;
@@ -161,7 +194,7 @@ class VL6180XSensor : public sensor::Sensor, public PollingComponent, public i2c
 	uint32_t gesture_hand_presence_threshold_;
 	uint32_t gesture_single_tap_threshold_;
 	uint32_t gesture_double_tap_threshold_; */
-	
+
     SensorState state_;
 
     // Declare triggers for the gestures
