6ca6fb4c75aae7b0efccc7f107c54455f32b5a4d
NonaSuomy <nonasuomy@live.com>
2025-07-19 21:16:25 -0400

    Add analog mic support

--- esphome/components/es8311/es8311.cpp
+++ esphome/components/es8311/es8311.cpp
@@ -35,59 +35,59 @@ void ES8311::setup() {
   ESP_LOGD(TAG, "ES8311 found at address 0x%02X", this->address_);
 
   // --- Initialization sequence from the working YAML example ---
-  ES8311_ERROR_FAILED(this->write_byte(0x44, 0x08));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG44_GPIO, 0x08));
   delay(10);
-  ES8311_ERROR_FAILED(this->write_byte(0x44, 0x08));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG44_GPIO, 0x08));
 
-  ES8311_ERROR_FAILED(this->write_byte(0x01, 0x30));
-  ES8311_ERROR_FAILED(this->write_byte(0x02, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x03, 0x10));
-  ES8311_ERROR_FAILED(this->write_byte(0x16, 0x24));
-  ES8311_ERROR_FAILED(this->write_byte(0x04, 0x10));
-  ES8311_ERROR_FAILED(this->write_byte(0x05, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x0B, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x0C, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x10, 0x1F));
-  ES8311_ERROR_FAILED(this->write_byte(0x11, 0x7F));
-  ES8311_ERROR_FAILED(this->write_byte(0x00, 0x80));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG01_CLK_MANAGER, 0x30));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG02_CLK_MANAGER, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG03_CLK_MANAGER, 0x10));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG16_ADC, 0x24));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG04_CLK_MANAGER, 0x10));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG05_CLK_MANAGER, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG0B_SYSTEM, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG0C_SYSTEM, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG10_SYSTEM, 0x1F));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG11_SYSTEM, 0x7F));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG00_RESET, 0x80));
   delay(10);
 
-  ES8311_ERROR_FAILED(this->write_byte(0x01, 0x3F));
-  ES8311_ERROR_FAILED(this->write_byte(0x06, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG01_CLK_MANAGER, 0x3F));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG06_CLK_MANAGER, 0x00));
 
-  ES8311_ERROR_FAILED(this->write_byte(0x13, 0x10));
-  ES8311_ERROR_FAILED(this->write_byte(0x1B, 0x0A));
-  ES8311_ERROR_FAILED(this->write_byte(0x1C, 0x6A));
-  ES8311_ERROR_FAILED(this->write_byte(0x44, 0x58));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG13_SYSTEM, 0x10));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG1B_ADC, 0x0A));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG1C_ADC, 0x6A));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG44_GPIO, 0x58));
 
   // --- Configuration for 16-bit, I2S Normal, 16kHz sample rate ---
-  ES8311_ERROR_FAILED(this->write_byte(0x09, 0x0C));
-  ES8311_ERROR_FAILED(this->write_byte(0x0A, 0x0C));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG09_SDPIN, 0x0C));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG0A_SDPOUT, 0x0C));
 
   // For 16kHz sample rate (assuming 12.288MHz MCLK)
-  ES8311_ERROR_FAILED(this->write_byte(0x02, 0x40));
-  ES8311_ERROR_FAILED(this->write_byte(0x05, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x03, 0x10));
-  ES8311_ERROR_FAILED(this->write_byte(0x04, 0x20));
-  ES8311_ERROR_FAILED(this->write_byte(0x07, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x08, 0xFF));
-  ES8311_ERROR_FAILED(this->write_byte(0x06, 0x03));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG02_CLK_MANAGER, 0x40));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG05_CLK_MANAGER, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG03_CLK_MANAGER, 0x10));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG04_CLK_MANAGER, 0x20));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG07_CLK_MANAGER, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG08_CLK_MANAGER, 0xFF));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG06_CLK_MANAGER, 0x03));
 
   // --- Enable codec (from es8311_start) ---
-  ES8311_ERROR_FAILED(this->write_byte(0x09, 0x0C));
-  ES8311_ERROR_FAILED(this->write_byte(0x0A, 0x0C));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG09_SDPIN, 0x0C));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG0A_SDPOUT, 0x0C));
 
-  ES8311_ERROR_FAILED(this->write_byte(0x17, 0xBF));
-  ES8311_ERROR_FAILED(this->write_byte(0x0E, 0x02));
-  ES8311_ERROR_FAILED(this->write_byte(0x12, 0x00));
-  ES8311_ERROR_FAILED(this->write_byte(0x14, 0x1A));
-  ES8311_ERROR_FAILED(this->write_byte(0x0D, 0x01));
-  ES8311_ERROR_FAILED(this->write_byte(0x15, 0x40));
-  ES8311_ERROR_FAILED(this->write_byte(0x37, 0x08));
-  ES8311_ERROR_FAILED(this->write_byte(0x45, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG17_ADC, 0xBF));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG0E_SYSTEM, 0x02));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG12_SYSTEM, 0x00));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG14_SYSTEM, 0x1A));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG0D_SYSTEM, 0x01));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG15_ADC, 0x40));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG37_DAC, 0x08));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG45_GP, 0x00));
   
   // Set microphone gain to 30dB (0x05) as in the working example
-  ES8311_ERROR_FAILED(this->write_byte(0x16, 0x05));
+  ES8311_ERROR_FAILED(this->write_byte(ES8311_REG16_ADC, 0x05));
 
   // Set initial volume
   this->set_volume(0.75);  // 0.75 = 0xBF = 0dB
